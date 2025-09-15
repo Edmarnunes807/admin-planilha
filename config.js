76856f643219e32fa536a847f048f349f1b9ee8a
Edmarnunes807 <edmarrocha807@gmail.com>
2025-09-15 19:51:27 -0400

    1.0

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Configurações do Web App do Google Apps Script
-const CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxbBrFQrcMxYBckQ9edsJ4XyQ4CYv06VTdYGMFRh4uapai3Wm7sZmRUtabR3LntnOPTtg/exec' // URL do Web App do Google Apps Script
-};
+// config.js - endpoint e token admin (se optar por guardar no front)
+const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbxvaysXKhPuTb4d7UL3_boSkQZmBkMSijg7TzGFigp5z91g-66fOZazKYCmsw3MQWk/exec"; // substitua pela sua URL do Apps Script
+// Se for servir este site num lugar seguro, insira o token aqui para facilitar:
+const ADMIN_TOKEN = "uma-string-secreta-aqui";
