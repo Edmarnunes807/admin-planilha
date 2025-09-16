b9284ebbc1394ab85b06cf3b421319a05039b7a1
Edmarnunes807 <edmarrocha807@gmail.com>
2025-09-15 20:20:34 -0400

    1.01

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js - configure sua URL do Apps Script (deploy admin)
-const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbx4Yb7he2aeCTbcCwIcOtf3tLXgWi4pdmu-_b9rS3pyBfAJ9OIUXjYJnjCfsFJ-nBgc/exec";
+const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbxHKcujQw71pOjGfDKzoIzBjtyfcJbJi_fPz_LmYQq3E2NAGBDArba65hPN3qVAxpUS/exec";
